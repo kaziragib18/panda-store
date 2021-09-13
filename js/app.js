febdd18e4dd78c6b5f3b153fc5d58efeb0fe7b2c
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-13 22:22:45 +0600

    debug fixed issues all fixed  UI design improved

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.image; //fixed from images to image    
+    const image = product.image; //debugged - images to image    
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product m-3 shadow p-3 pb-2 mb-5 bg-body rounded">
-      <div class="pl-2 m-3 w-100">
+      <div class="pl-2 m-2 w-100">
     <img class="product-image" src="${image}"></img>
       </div>
       <br>
@@ -25,11 +25,10 @@ const showProducts = (products) => {
       <hr>
       <h2 class="fs-6 pb-2 fw-bold">Price:<span class="text-danger"> $ ${product.price}</h2>
       <hr>
-      <h2 class="fs-6 pb-2 fw-bold">Rating:<span class="text-primary"> ${product.rating.rate}<span class="text-warning"> <i class="fas fa-star"></i></span></h2>
+      <h2 class="fs-6 pb-2 fw-bold">Rating:<span class="text-primary"> ${product.rating.rate}<span class="text-warning"><i class="fas fa-star"></i></span> / <span class="text-dark">Reviews: <span class="text-primary">${product.rating.count}</span> <img class="p-1 mb-2" width="30px" src="image/review.svg"></h2>
       <hr>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-dark">Add To Cart</button>
-
-      <button id="details-btn" class="btn btn-danger">Details</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-dark fst-italic rounded rounded-pill pl-2">Add To Cart</button>
+      <button id="details-btn" class="btn btn-danger rounded rounded-pill fst-italic pl-2">Details</button>
       </div>
       </div>
       `;
@@ -42,28 +41,28 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal();  //debugged - updateTotal wasn't called
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseFloat(element); //debugged here to parseInt to FLoat
+  const converted = parseFloat(element); //debugged - parseInt to parseFLoat
   return converted;
 };
 
-// console.log(converted);
 
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.fround(total).toFixed(2);
+  document.getElementById(id).innerText = Math.fround(total).toFixed(2); //decimal point fix
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.fround(value).toFixed(2);
+  document.getElementById(id).innerText = Math.fround(value).toFixed(2); //decimal point fix
 };
 
 // update delivery charge and total Tax
@@ -85,8 +84,9 @@ const updateTaxAndCharge = () => {
 
 //grandTotal update function
 const updateTotal = () => {
+  // const price = document.getElementById('price').value;
+  // console.log(price);
   const grandTotal = getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2); //total decimal point fix
 
 };
-// console.log(grandTotal);
